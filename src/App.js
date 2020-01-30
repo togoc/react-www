c4bdbda7ba95d6fd452175cd5ccab59f2a9bb642
togoc <oncwnuDRPW2KUww1bxVRTJZJIaq0@git.weixin.qq.com>
2020-01-31 01:41:04 +0800

    主页布局

--- src/App.js
+++ src/App.js
@@ -7,21 +7,20 @@ import "./App.less"
 class App extends React.Component {
   render() {
     let token = localStorage.getItem('USER_TOKEN')
-    console.log(token)
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <Route render={(props) => {
+          {/* <Route render={(props) => {
             //可以通过render方法传值
             //可以判断是否登录返回页面
             if (!token)
               return <Redirect to="/login" />
-          }} path="/" />
+          }} path="/" /> */}
           <Switch>
             <Route path="/home" component={Home}></Route>
             <Route path="/admin" component={Admin}></Route>
-            <Route path="/login" component={Login}></Route>
-            <Route path="/register" component={Register}></Route>
+            <Route path="/login" component={Login} exact></Route>
+            <Route path="/register" component={Register} exact></Route>
           </Switch>
         </BrowserRouter>
       </Provider>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,23 +1,70 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import './index.less'
 // import * as Actions from '../../store/actions/article'
 class index extends Component {
+    state = {
+        demoList: [
+            {
+                id: 1,
+                title: 'demo1',
+                main: 'react',
+                link: 'www',
+                pic: ['http://106.13.184.92/mallshop/img/swiper/banner3.png']
+            },
+            {
+                id: 2,
+                title: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, perferendis.',
+                link: 'www',
+                main: 'react',
+                pic: ['http://106.13.184.92/mallshop/img/swiper/banner3.png']
+            },
+            {
+                id: 3,
+                main: 'vue',
+                title: 'demo3',
+                link: 'www',
+                pic: ['http://106.13.184.92/mallshop/img/swiper/banner3.png']
+            },
+            {
+                id: 3,
+                main: 'vue',
+                title: 'demo3',
+                link: 'www',
+                pic: ['http://106.13.184.92/mallshop/img/swiper/banner3.png']
+            }
+        ]
+    }
     componentDidMount() {
         let token = localStorage.getItem('USER_TOKEN')
         console.log(token)
     }
     render() {
-        let token = localStorage.getItem('USER_TOKEN')
-        console.log(token)
-        // setTimeout(() => {
-        //     console.log(localStorage.getItem('USER_TOKEN'))
-        // }, 1000);
-        // if (!token)
-        //     return <Redirect to="/login" />
         return (
-            <div>
-                index
+            <div className="home">
+                {
+                    this.state.demoList.map(v => {
+                        return (
+                            <div className="demo_item" style={{ backgroundImage: `url(${v.pic[0]})` }}>
+                                <div className="demo_item_detail">
+                                    <div className="title-outer">
+                                        <span className="title">
+                                            {v.title}
+                                        </span>
+                                    </div>
+
+                                    <div className="main-outer">
+                                        <span className="main">
+                                            {v.main}
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="bg"></div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
